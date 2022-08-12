3682d05af6c638a5ca145cb2e70f245d55236ff8
cirligelena <74189787+cirligelena@users.noreply.github.com>
2022-08-12 16:12:37 +0300

    Lena task1 (#3)
    * get all books, authors, categories
    
    * book a book function added

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,46 @@
+import {PulseLoader} from "react-spinners";
+import React, {useEffect, useState } from "react";
+import NavigationComponent from "../navigation/Navigation";
+
+
+
+const LoaderComponent = ({divToLoad}) => {
+    const [isLoading, setLoading] = useState(true);
+    const [divLoaded] = useState(divToLoad);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 800)
+    }, []);
+
+    const executeLoader = (loader, divLoaded) => {
+        return (
+            <>
+                {
+                    isLoading ?
+                        <PulseLoader cssOverride={{
+                            textAlign: "center",
+                            paddingTop: "20%"
+                        }}
+                                     size={25} />
+                        : <>
+                            <NavigationComponent />
+                            {divLoaded}
+                        </>
+
+                }
+            </>
+        );
+
+    };
+
+
+    return (
+        <div>
+            {executeLoader(<PulseLoader />, divLoaded)}
+        </div>
+    );
+};
+
+export default LoaderComponent;
