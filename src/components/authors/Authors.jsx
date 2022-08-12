d6fa6e6c705e2aad79a9b90fdb6590f3b1ff4886
cirligelena <cirlig.elena@mail.ru>
2022-08-12 10:12:44 +0300

    get all books, authors, categories

--- src/components/authors/Authors.jsx
+++ src/components/authors/Authors.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import {fetchAuthorList} from "../../redux/actions/author";
+import {getAuthorList} from "../../redux/selectors/author";
 
 
 const AuthorsComponent = () => {
+    const dispatch = useDispatch();
+    dispatch(fetchAuthorList());
+
+    const authors = useSelector(getAuthorList);
     return (
         <div>
-            <h2>Authors Page</h2>
-        </div>
-    );
+            <ul>
+                {authors?.map((result) =>
+                    <Card style={{ width: '18rem' }} key={result.id}>
+                        <Card.Body>
+                            <Card.Title>{result.firstName} {result.lastName}</Card.Title>
+                            <Card.Text>{result.biography}</Card.Text>
+                        </Card.Body>
+                        <Button variant="primary">Show author's books</Button>
+                    </Card>)}
+            </ul>
+        </div>);
 };
 
 export default AuthorsComponent;
---
+++ src/redux/selectors/category.js
@@ -0,0 +1,1 @@
+export const getCategoryList = (state) => state.allCategories.categoryList;
