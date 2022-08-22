4f171599868f3e09b93753884f97f2db31c0b02d
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-08-22 15:34:05 +0300

    Cleaning up on category and authors + styles
    1. Decomponing authors component and category component
    2. Added styles for authors cards with responsobility

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,47 +1,34 @@
-
 import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import {fetchCategoryList} from "../../redux/actions/category";
 import {getCategoryList} from "../../redux/selectors/category";
-import LoaderComponent from "../loader/Loader";
+import ServerNotRespondingErrorComponent from "../errors/ServerNotRespondingError";
+import CategoriesCards from "./CategoriesCards";
 
 
 const CategoriesComponent = () => {
     const [loaded, setLoaded] = useState(false)
     const dispatch = useDispatch();
+    const categories = useSelector(getCategoryList);
 
     useEffect(() => {
+        setLoaded(false);
+
         dispatch(fetchCategoryList()).then(() => {
             setLoaded(true)
         })
-    }, []);
-
-    const categories = useSelector(getCategoryList);
+    }, [dispatch]);
 
 
     return (
         <>
-            {loaded ? <LoaderComponent divToLoad={
-                <div>
-                    <ul>
-                        {Array.isArray(categories)
-                            ? categories.map(result => {
-                                return (
-                                    <Card style={{width: '18rem'}} key={result.id}>
-                                        <Card.Body>
-                                            <Card.Title>{result.title}</Card.Title>
-                                        </Card.Body>
-                                        <Button variant="primary">Show books of this category</Button>
-                                    </Card>)
-                            })
-                             : <div> No items found </div>
-                       }
-                    </ul>
-                </div>
-            }/> : <div></div>
-            }
+            <>
+                {loaded ?
+                    <CategoriesCards categories={categories}/>
+                    :
+                    <ServerNotRespondingErrorComponent/>
+                }
+            </>
         </>
     );
 };
