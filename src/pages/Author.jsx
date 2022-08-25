d4ad7f5dbbd3505a2ee9ea0e68d3b5f7dea13651
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-08-25 08:40:53 +0300

    Added pages

---
+++ src/pages/Author.jsx
@@ -0,0 +1,14 @@
+import '../assets/styles/authors.css';
+import AllAuthors from "../components/authors/AllAuthors";
+
+
+const AuthorPage = () => {
+    return (
+        <div className="author-page">
+            <AllAuthors />
+        </div>
+    )
+}
+
+
+export default AuthorPage;
---
+++ src/pages/Categories.jsx
@@ -0,0 +1,12 @@
+import AllCategories from "../components/categories/AllCategories"
+
+
+const CategoriesPage = () => {
+    return (
+        <div className="categories-page">
+            <AllCategories />
+        </div>
+    )
+}
+
+export default CategoriesPage;
