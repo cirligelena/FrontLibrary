d297f3d88b1792bae22e9bfa987e71933cdd3a59
dariacucicovscaia <91362775+dariacucicovscaia@users.noreply.github.com>
2022-09-07 17:06:14 +0300

    sessionFinished fix

--- src/components/auth/SessionFinished.js
+++ src/components/auth/SessionFinished.js
@@ -1,37 +1,44 @@
 import React, {useEffect, useState} from "react";
-import { useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {Modal} from "react-bootstrap";
 import {getShowModalState} from "../../redux/selectors/flagSelectors";
-
 
 
 const SessionFinished = ({children}) => {
     const showModal = useSelector(getShowModalState);
     const navigate = useNavigate();
     const [showpopup, setShowpopup] = useState(false)
-    const handlePopUp = (e) => {setShowpopup(!showpopup)};
+    const handlePopUp = (e) => {
+        setShowpopup(!showpopup)
+    };
     useEffect(() => {
-        navigate("/");
-          if (showModal) {setShowpopup(true)}
+        if (showModal) {
+            setShowpopup(true)
+            navigate("/");
+        }
     }, [showModal])
 
-    return  (
+    return (
         <>
-    <div>
-        {children}
-    </div>
-    <Modal show={showpopup} onHide={handlePopUp}>
-        <Modal.Header closeButton>
-            <Modal.Title>Session finished </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Please login again</Modal.Body>
-        <Modal.Footer>
-            <button className="card-btn100__buttons" onClick={() => {navigate("/login");  handlePopUp()}}>Login</button>
-        </Modal.Footer>
-    </Modal>
+            <div>
+                {children}
+            </div>
+            <Modal show={showpopup} onHide={handlePopUp}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Session finished </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Please login again</Modal.Body>
+                <Modal.Footer>
+                    <button className="card-btn100__buttons" onClick={() => {
+                        navigate("/login");
+                        handlePopUp()
+                    }}>Login
+                    </button>
+                </Modal.Footer>
+            </Modal>
         </>
-)
+    )
 }
 
 export default SessionFinished;
